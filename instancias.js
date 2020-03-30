86d8bd1326fd8b4dc35d62072d11f61d37fdf1e6
diazfabio18 <fabiodiaz@est.frba.utn.edu.ar>
2020-03-29 23:21:49 -0300

    version 4.0

---
+++ instancias.js
@@ -0,0 +1,228 @@
+new Vue({
+    el: '#appp',
+    data: {
+        lista_numeros: [
+            {
+                
+                fecha: '25.2 - 29/03/20',
+                numero: './numbers/25_2number.gif',
+                alt: 25.2,
+                enlace: '',
+                parrafo: 'Especial por extensión de la cuarentena'
+            },
+            {
+                
+                fecha: '25.1 - 25/03/20',
+                numero: './numbers/25_1number.jpg',
+                alt: 25.1,
+                enlace: '',
+                parrafo: ''
+            },
+            {
+                
+                fecha: '25 - 26/03/20 ',
+                numero: './numbers/25number.png',
+                alt: 25,
+                enlace: '',
+                parrafo: ''
+            },
+            {
+                
+                fecha: '24 - 25/03/20',
+                numero: './numbers/24number.mp4',
+                alt: 24,
+                enlace: 'video',
+                parrafo: 'Je t\'aime by Richard Clayderman'
+            },
+            {
+                
+                fecha: '23 - 23/03/20',
+                numero: './numbers/23number.png',
+                alt: 23,
+                enlace: '',
+                parrafo: 'Creación de Tmotn'
+            },
+            {
+                
+                fecha: '22 - 19/03/20',
+                numero: './numbers/22number.mp4',
+                alt: 22,
+                enlace: 'video'
+            },
+            {
+                
+                fecha: '21 - 14/03/20',
+                numero: './numbers/21number.jpg',
+                alt: 21,
+                enlace: ''
+            },
+            {
+                
+                fecha: '20 - 11/03/20',
+                numero: './numbers/20number.jpg',
+                alt: 20,
+                enlace: ''
+            },
+            {
+                
+                fecha: '19 - 06/03/20',
+                numero: './numbers/19number.jpg',
+                alt: 19,
+                enlace: ''
+            },
+            {
+                
+                fecha: '18 - 02/03/20',
+                numero: './numbers/18number.jpg',
+                alt: 18,
+                enlace: ''
+            },
+            {
+                
+                fecha: '17 - 28/02/20',
+                numero: './numbers/17number.jpg',
+                alt: 17,
+                enlace: ''
+            },
+            {
+                
+                fecha: '16 - 24/02/20',
+                numero: './numbers/16number.jpg',
+                alt: 16,
+                enlace: ''
+            },
+            {
+                
+                fecha: '15 - 22/02/20',
+                numero: './numbers/15number.jpg',
+                alt: 15,
+                enlace: ''
+            },
+            {
+                
+                fecha: '14 - 14/02/20',
+                numero: './numbers/14number.png',
+                alt: 14,
+                enlace: 'https://diazfabio18.github.io/San_Valentin/',
+                parrafo: 'Feliz San Valentin!'
+            },
+            {
+                
+                fecha: '13 - 11/02/20',
+                numero: './numbers/13number.jpg',
+                alt: 13,
+                enlace: '',
+                parrafo: 'Feliz cumple! (adelantado)'
+            },
+            {
+                
+                fecha: '12 - 13/02/20',
+                numero: './numbers/12number.jpg',
+                alt: 12,
+                enlace: ''
+            },
+            {
+                
+                fecha: '11 - 12/02/20',
+                numero: './numbers/11number.jpg',
+                alt: 11,
+                enlace: ''
+            },
+            {
+                
+                fecha: '10 - 11/02/20',
+                numero: './numbers/10number.jpg',
+                alt: 10,
+                enlace: ''
+            },
+            {
+                
+                fecha: '9 - 10/02/20',
+                numero: './numbers/9number.jpg',
+                alt: 9,
+                enlace: ''
+            },
+            {
+                
+                fecha: '8 - 08/02/20',
+                numero: './numbers/8number.jpg',
+                alt: 8,
+                enlace: ''
+            },
+            {
+                
+                fecha: '7 - 07/02/20',
+                numero: './numbers/7number.jpg',
+                alt: 7,
+                enlace: ''
+            },
+            {
+                
+                fecha: '6 - 05/02/20',
+                numero: './numbers/6number.jpg',
+                alt: 6,
+                enlace: ''
+            },
+            {
+                
+                fecha: '5 - 04/02/20',
+                numero: './numbers/5number.jpg',
+                alt: 5,
+                enlace: ''
+            },
+            {
+                
+                fecha: '4 - 02/02/20',
+                numero: './numbers/4number.jpg',
+                alt: 4,
+                enlace: ''
+            },
+            {
+                
+                fecha: '3 - 24/01/2020',
+                numero: './numbers/3number.jpg',
+                alt: 3,
+                enlace: ''
+            },
+            {
+                fecha: '2 - 17/01/20',
+                numero: './numbers/2number.jpg',
+                alt: 2,
+                enlace: ''
+            
+            },
+            {
+                fecha: '1 - ??? ',
+                numero: './numbers/mystery.jpg',
+                alt: 'misterio',
+                enlace: '',
+                parrafo: '(Cuando nos veamos)'
+            }
+        ],
+        destacados: [
+            {
+                
+                fecha: '24 - 25/03/20',
+                numero: './numbers/24number.mp4',
+                alt: 24,
+                enlace: 'video',
+                parrafo: 'Je t\'aime by Richard Clayderman'
+            },
+            {
+                
+                fecha: '25.2 - 29/03/20',
+                numero: './numbers/25_2number.gif',
+                alt: 25.2,
+                enlace: '',
+                parrafo: 'Especial por extensión de la cuarentena'
+            },
+            {
+                fecha: '1 - ???',
+                numero: './numbers/mystery.jpg',
+                alt: 'misterio',
+                enlace: '',
+                parrafo: '(Cuando nos veamos)'
+            }
+        ]
+    }
+})
